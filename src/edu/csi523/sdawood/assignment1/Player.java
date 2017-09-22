4fb8d777ebff2a36d9290660617e4877a08a7957
Shova Shrestha <shova.shrestha977@gmail.com>
2017-09-22 18:58:50 -0400

    code cleanup

--- src/edu/csi523/sdawood/assignment1/Player.java
+++ src/edu/csi523/sdawood/assignment1/Player.java
@@ -5,23 +5,36 @@ import java.applet.Applet;
 
 public class Player {
 
-    DataCell cells = new DataCell();
+    DataCell[] cells = new DataCell[9];
     String name;
     String symbol;
 
-    public void MarkCell(int row, int col){
-   /* if ((row >= 0) && (row < 3)){
-        if((col >= 0) && (col < 3)){
-            if (board [row][col] == 'x'){
-                board [row][col] = currentPlayerMark;
+    private Player Players[];
+    private int currentPlayer;
+    private char myMark;
+    private String[][] players = { { "Player 1", "X" }, { "Player 2", "O" } };
+
+
+    @Override
+    public void MarkCell(int row, int col) {
+        if ((row >= 0) && (row < 3)) {
+            if ((col >= 0) && (col < 3)) {
+                if (board[row][col] == 'x') {
+                    board[row][col] = currentPlayerMark;
+                }
             }
         }
-    }*/
-
     }
+   /*enum Mark{
+       X, O;
+   }*/
+
 
     public Player(String name, String symbol){
+        players = new Player[];
+        currentPlayer = 0;
 
+        symbol = (num == 0 ? 'X' : 'O');
     }
 
     public void register(){
